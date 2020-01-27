3f519e6bbd2709e7f18d867df649bb9ae92886e4
Thijsvl12 <thijsvl12@gmail.com>
2020-01-27 13:52:10 +0100

    Update API

--- Artikelen_API/index.js
+++ Artikelen_API/index.js
@@ -5,7 +5,7 @@ let bodyparser = require('body-parser')
 
 
 let app = express()
-let db = mongojs('Artikelen', [])
+let db = mongojs('Artikelen', ['artikelen', 'orders'])
 let objectID = mongodb.ObjectID
 let port = process.env.PORT || 3001;
 
@@ -16,8 +16,12 @@ app.use(
   })
 );
 
-app.get('/', function(req, res){
-    return res.json({'message' :'Api Working'})
+app.post('/iArtikel', function(req, res){
+    let artikel = req.body.artikel
+    db.artikelen.insert(artikel, function(err, docs){
+        if(err)return res.json({mesage:'error', error: err})
+        else return res.json({message:'Artikel Added', docs: docs})
+    })
 })
 
 app.listen(port, function() {
